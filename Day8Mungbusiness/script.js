5fbfc4c63443cb4d3a12880b464113a2c7ed9618
JIHEA LEE <petallon94@naver.com>
2024-06-28 16:41:13 +0900

    Add files via upload
    first upload

---
+++ Day8Mungbusiness/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 300) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    });
+
+
+    scrollToTopBtn.addEventListener("click", function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+});
